0e10e34b9673ed0d2ac259e4b4f1879b8eef7933
Nirbhik16 <co20335@ccet.ac.in>
2023-02-02 21:13:00 +0530

    test

---
+++ Nirbhik Kakkar/test.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        bool flag=false;
+        int count=0;
+        int n=s.size();
+        for(int i=n-1;i>=0;i--){
+            if(s[i]!=' '){
+                flag=true;
+                count++;
+            }
+            else if(s[i]==' ' && flag==true){
+                return count;
+            }
+        }
+        return count;
+    }
+};
