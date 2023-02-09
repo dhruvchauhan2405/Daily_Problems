f21d3a7fcd3ccfe6aa669fa0bc2481f5e285c914
Jeevan <pandeyjeevan17@gmail.com>
2023-02-09 17:25:18 +0530

    Remove Element sol

--- Jeevan Pandey/Day1/Remove Element/sol.cpp
+++ Jeevan Pandey/Day1/Remove Element/sol.cpp
@@ -1,1 +1,14 @@
-kjhkfdkjhkjfkjsdhfkjskjfkjsgdjkfgkjgsdf
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int n=0;
+    for( int i=0; i<nums.size(); i++)
+    {
+        if (nums[i]!=val)
+        {
+            nums[n++] = nums[i];
+        }
+    }
+    return n;
+}
+};
