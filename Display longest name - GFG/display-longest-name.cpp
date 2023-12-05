70ddca8c25930ed253b650601d3fa740f7fa36ab
Dhruv Singh Chauhan <101703455+dhruvchauhan2405@users.noreply.github.com>
2023-12-05 15:04:23 +0530

    Added solution - LeetHub

---
+++ Display longest name - GFG/display-longest-name.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    string longest(string names[], int n)
+    {
+        string s;
+        int len=0;
+        for(int i=0;i<n;i++){
+            if(names[i].length()>len){
+                len=names[i].length();
+                s=names[i];
+            }
+            
+        }
+        return s;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		string names[n];
+		
+		for(int i=0;i<n;i++)
+			cin>>names[i];
+		Solution ob;
+		cout<< ob.longest(names, n) << endl;
+	}
+}
+
+// } Driver Code Ends
